b816dde564d0a16ef79ea56c722b2740a6b3463c
Liavch <Liavch@gmail.com>
2024-08-05 22:54:41 +0300

    Add files via upload

---
+++ index-js.js
@@ -0,0 +1,11 @@
+import React from 'react';
+   import ReactDOM from 'react-dom';
+   import ShiftManagementSystem from './shift-management-system';
+
+   ReactDOM.render(
+     <React.StrictMode>
+       <ShiftManagementSystem />
+     </React.StrictMode>,
+     document.getElementById('root')
+   );
+   
